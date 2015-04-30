f926168106bd279a19eafd564ec181238c859dbc
mosuke5 <w.vamos603@gmail.com>
2015-04-30 03:27:37 -0400

    vimrcファイルの権限を変更

--- data/bootvim.sh
+++ data/bootvim.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cp /home/you/vimrcs/vimrc_default /home/you/vimrcs/vimrc_`hostname`
-chown vimfactory:vimfactory /home/you/vimrcs/vimrc_`hostname`
+chmod 664 /home/you/vimrcs/vimrc_`hostname`
 su - you
