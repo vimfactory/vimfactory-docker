18f3526b4901bbd95a62037c7b91f4fabe793fbb
mosuke5 <w.vamos603@gmail.com>
2016-01-11 14:24:03 +0900

    プラグイン機能追加にあたり、/etc/vimrcへのruntimepathの追加とvimstorageの権限修正1

--- data/bootvim.sh
+++ data/bootvim.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 mkdir -p /home/you/vimstorage/`hostname`/runtimepath/plugin
+chmod -R 775 /home/you/vimstorage/`hostname`
 cp /home/you/vimstorage/default/vimrc /home/you/vimstorage/`hostname`/vimrc
 chmod 664 /home/you/vimstorage/`hostname`/vimrc
+
+# /etc/vimrcにオリジナルのruntimepathを追記
+echo "set runtimepath+=/home/you/vimstorage/`hostname 2> /dev/null`/runtimepath" >> /etc/vimrc
 su - you
